4380b06f85969e12676e23daa39321a60ef8ba42
Keesha Hargrove <kharg14@gmail.com>
2020-07-25 00:59:40 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,21 @@
 ## code your solution here. 
+class Cat
+  def initialize(name, age, location)
+    @name = name
+    @age = age
+    @location = location
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def speak
+    puts "Meow!"
+  end
+
+end
+
+
+
